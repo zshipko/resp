c189135ed533bd044a2a7bb1002e5858d2b720e3
Zach Shipko <zachshipko@gmail.com>
2018-12-13 16:57:19 -0800

    Fix example

--- examples/mirage/config.ml
+++ examples/mirage/config.ml
@@ -2,17 +2,10 @@ open Mirage
 
 let main =
   foreign
-  ~packages:[
-    package "duration";
-    package "resp-lwt-mirage";
-    package "mirage-conduit";
-  ]
-  ~deps:[
-    abstract nocrypto;
-  ]
-  "Unikernel.Main" (pclock @-> conduit @-> job)
-
+    ~packages:
+      [package "duration"; package "resp-mirage"; package "mirage-conduit"]
+    ~deps:[abstract nocrypto] "Unikernel.Main"
+    (pclock @-> conduit @-> job)
 
 let server = generic_stackv4 default_network |> conduit_direct
-
 let () = register "resp-server" [main $ default_posix_clock $ server]

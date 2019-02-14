91f9beec9516ae5d794f687f47374900d8676c94
Zach Shipko <zachshipko@gmail.com>
2019-02-14 09:46:13 -0800

    Add empty strings to test

--- test/test.ml
+++ test/test.ml
@@ -2,13 +2,13 @@ module Value = Resp.String
 open Lwt.Infix
 
 let test_roundtrip _ () =
-  let v = `Array [|`String "x"; `Integer 123L; `Bulk "abc"|] in
+  let v = `Array [|`String "x"; `Integer 123L; `Bulk "abc"; `String ""; `Bulk ""; `Array [||]; `Array [| `Integer 1L |]|] in
   let output = ref "" in
   Value.write output v
   >>= fun () ->
   Value.read output
-  >|= fun v' -> Alcotest.(check bool) "roundtrip" (v = v') true
+  >|= fun v' -> Alcotest.(check bool) "compare" (v = v') true
 
 let () =
   Alcotest.run "Resp"
-    [("encoding", [Alcotest_lwt.test_case "Get" `Quick test_roundtrip])]
+    [("encoding", [Alcotest_lwt.test_case "Roundtrip" `Quick test_roundtrip])]

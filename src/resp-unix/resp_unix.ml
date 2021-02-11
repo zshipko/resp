93ef77c7eac714e2e7a8640906fef0313e80a24c
Zach Shipko <zachshipko@gmail.com>
2021-02-10 20:54:35 -0800

    WIP

--- src/resp-unix/resp_unix.ml
+++ src/resp-unix/resp_unix.ml
@@ -4,7 +4,9 @@ module Reader = Resp.Reader (struct
   type ic = Lwt_io.input_channel
 
   let read ic n = Lwt_io.read ic ~count:n
+
   let read_char = Lwt_io.read_char
+
   let read_line ic = Lwt_io.read_line ic
 end)
 
@@ -21,7 +23,9 @@ struct
   include Data
 
   type ic = Lwt_io.input_channel
+
   type oc = Lwt_io.output_channel
+
   type server = Conduit_lwt_unix.ctx * Conduit_lwt_unix.server
 
   let run server fn =
@@ -35,7 +39,9 @@ module Client_backend = struct
   open Lwt.Infix
 
   type ic = Lwt_io.input_channel
+
   type oc = Lwt_io.output_channel
+
   type params = Conduit_lwt_unix.ctx * Conduit_lwt_unix.client
 
   let connect params =
@@ -46,11 +52,16 @@ end
 module Server = struct
   module Make
       (Auth : Resp_server.AUTH) (Data : sig
-          type data
+        type data
       end) =
     Resp_server.Make (Backend (Data)) (Auth) (Resp.Make (Reader) (Writer))
 
-  module Default = Make (Resp_server.Auth.String) (struct type data = unit end)
+  module Default =
+    Make
+      (Resp_server.Auth.String)
+      (struct
+        type data = unit
+      end)
 end
 
 module Client = Resp_client.Make (Client_backend) (Resp.Make (Reader) (Writer))

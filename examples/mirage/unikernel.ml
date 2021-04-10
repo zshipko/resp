8ffd2bd86d4c162cd9d6a1602af00943fa39bf40
zach <zachshipko@gmail.com>
2021-04-10 12:06:10 -0700

    Fix unikernel example

--- examples/mirage/unikernel.ml
+++ examples/mirage/unikernel.ml
@@ -1,21 +1,21 @@
 open Lwt.Infix
-open Mirage_types_lwt
-module Server = Resp_mirage.Server.Default
-
-let commands =
-  [
-    ( "ping",
-      fun _ client cmd nargs ->
-        if nargs = 0 then Server.ok client
-        else Server.recv client >>= fun x -> Server.send client x );
-  ]
+open Mirage_clock
 
 module Main (Clock : PCLOCK) (Conduit : Conduit_mirage.S) = struct
+  module R = Resp_mirage.Make (Conduit)
+  module Server = R.Server.Default
+
+  let commands =
+    [
+      ( "ping",
+        fun _ client cmd nargs ->
+          if nargs = 0 then Server.ok client
+          else Server.recv client >>= fun x -> Server.send client x );
+    ]
+
   let start clock conduit =
     let port = 8888 in
-    let addr = Ipaddr.of_string_exn "0.0.0.0" in
-    let endp = `TCP (addr, port) in
-    Conduit_mirage.server endp >>= fun server ->
+    let server = `TCP port in
     Logs.info (fun f -> f "Starting server on port 8888");
     let server = Server.create ~commands (conduit, server) () in
     Server.start server
